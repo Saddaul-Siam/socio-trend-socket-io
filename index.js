57f89945e4c8fecfc7a1c48cb0bf07b812b4f8fa
saddaul-siam <saddadul.siam@gmail.com>
2022-03-15 00:06:09 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const io = require("socket.io")(8900, {
+const port = process.env.PORT || 8900;
+
+const io = require("socket.io")(port, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "https://socio-trend.vercel.app/",
   },
 });
 
