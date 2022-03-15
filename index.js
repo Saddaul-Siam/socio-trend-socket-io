09104997715d54208d3f9e2ed807e8c6ffb3bfb7
saddaul-siam <saddadul.siam@gmail.com>
2022-03-15 17:22:55 +0600

    try to live

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000" || "https://socio-trend.vercel.app",
+    origin: "https://socio-trend.vercel.app",
   },
 });
 
